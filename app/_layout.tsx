d50683e86882fabe96c90d19a35749fa794dc601
richmondnkrumah04 <richmondnkrumah04@gmail.com>
2025-10-25 01:25:47 +0000

    update: added auth flow with store

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,26 @@
 import { Stack } from "expo-router";
-import * as SplashScreen from 'expo-splash-screen'
+import * as SplashScreen from "expo-splash-screen";
 import { useEffect, useState } from "react";
-
-SplashScreen.setOptions({
-  duration: 1000,
-  fade: true
-})
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useAuthStore } from "@/utils/authStore";
+import { supabase } from "@/lib/supabase";
+import { Session } from "@supabase/supabase-js";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [toHide,SetToHide] = useState(false)
+  const { isLoggedIn, hasCompletedOnboarding } = useAuthStore();
+  const [hideSplash, setHideSplash] = useState(true);
+  const { saveUserSession } = useAuthStore();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      saveUserSession(session!);
+    });
+    supabase.auth.onAuthStateChange((_event, session) => {
+      saveUserSession(session!);
+    });
+  }, []);
   useEffect(() => {
     if(!toHide){
       setTimeout(()=>{
