07a7a54d3ae1b2aaeeefce43f3d359c2877589b3
richmondnkrumah04 <richmondnkrumah04@gmail.com>
2025-11-01 02:15:29 +0000

    update: initial screen look

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const list = () => {
   return (
-    <View>
-      <Text>list</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Shopping List</Text>
     </View>
-  )
-}
+  );
+};
 
-export default list
+export default list;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: (StatusBar.currentHeight ?? 0) + 15,
+    paddingHorizontal: 25,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#5C493D",
+    paddingTop: 20,
+    paddingBottom: 10,
+  },
+});
