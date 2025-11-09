58be84dcdaa292b28625628155eac327d7fef4c9
richmondnkrumah04 <richmondnkrumah04@gmail.com>
2025-11-09 03:20:25 +0000

    Nov 9, 2025, 3:20 AM

---
+++ app/(tabs)/plan/_layout.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const PlanRootLayout = () => {
+  return (
+<Stack screenOptions={{
+    headerShown: false,
+}}/>
+)
+}
+
+export default PlanRootLayout
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/plan/recipe/[recipe].tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SingleRecipeScreen = () => {
+  return (
+    <View>
+      <Text>SingleRecipeScreen</Text>
+    </View>
+  )
+}
+
+export default SingleRecipeScreen
+
+const styles = StyleSheet.create({})
