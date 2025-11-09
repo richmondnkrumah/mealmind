08bdb2bb0dff47dd48047134d80ab83d441d5750
richmondnkrumah04 <richmondnkrumah04@gmail.com>
2025-11-09 03:26:00 +0000

    Nov 9, 2025, 3:26 AM

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -4,6 +4,7 @@ import { Image } from "expo-image";
 import type { DAYMEALS, RECIPE } from "@/constants/types";
 import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
 import { PRIMARY } from "@/constants/colors";
+import { Link } from "expo-router";
 
 const RecipeCard = ({
   recipe,
@@ -12,54 +13,59 @@ const RecipeCard = ({
   recipe: RECIPE | null;
   mealType: keyof DAYMEALS;
 }) => {
-  console.log(recipe)
+  console.log(recipe);
   return (
-    <View style={styles.recipeContainer}>
-      <Text style={styles.recipeCategory}>{mealType}</Text>
-      {!recipe ? (
-        <View style={[styles.recipeCard, styles.emptyCard]}>
-          <Ionicons name="restaurant-outline" size={48} color={PRIMARY} />
-          <Text style={styles.emptyCardText}>
-            No meal planned for this slot.
-          </Text>
-        </View>
-      ) : (
-        <View style={styles.recipeCard}>
-          <Image
-            source={recipe.imageUrl}
-            style={{
-              height: 180,
-              width: "100%",
-            }}
-            contentFit="cover"
-          />
-          <View
-            style={{
-              paddingHorizontal: 20,
-              paddingVertical: 5,
-              gap: 10,
-            }}
-          >
-            <Text style={styles.title}>{recipe.recipeName}</Text>
+    <Link href={{
+      pathname: '/plan/recipe/[data]',
+      params: { data:JSON.stringify(recipe) }
+    }} asChild>
+      <View style={styles.recipeContainer}>
+        <Text style={styles.recipeCategory}>{mealType}</Text>
+        {!recipe ? (
+          <View style={[styles.recipeCard, styles.emptyCard]}>
+            <Ionicons name="restaurant-outline" size={48} color={PRIMARY} />
+            <Text style={styles.emptyCardText}>
+              No meal planned for this slot.
+            </Text>
+          </View>
+        ) : (
+          <View style={styles.recipeCard}>
+            <Image
+              source={recipe.imageUrl}
+              style={{
+                height: 180,
+                width: "100%",
+              }}
+              contentFit="cover"
+            />
             <View
               style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                gap: 10,
               }}
             >
-              <View style={styles.iconContainer}>
-                <SimpleLineIcons name="fire" size={22} color={PRIMARY} />
-                <Text style={styles.iconText}>{recipe.calories}</Text>
-              </View>
-              <View style={styles.iconContainer}>
-                <SimpleLineIcons name="clock" size={20} color={PRIMARY} />
-                <Text style={styles.iconText}>{recipe.cookTime}</Text>
+              <Text style={styles.title}>{recipe.recipeName}</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <View style={styles.iconContainer}>
+                  <SimpleLineIcons name="fire" size={22} color={PRIMARY} />
+                  <Text style={styles.iconText}>{recipe.calories}</Text>
+                </View>
+                <View style={styles.iconContainer}>
+                  <SimpleLineIcons name="clock" size={20} color={PRIMARY} />
+                  <Text style={styles.iconText}>{recipe.cookTime}</Text>
+                </View>
               </View>
             </View>
           </View>
-        </View>
-      )}
-    </View>
+        )}
+      </View>
+    </Link>
   );
 };
 
@@ -78,12 +84,12 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     overflow: "hidden",
     borderWidth: 1,
-    borderColor: "#E0E0E0",
+    borderColor: "#red",
   },
   emptyCard: {
     justifyContent: "center",
     alignItems: "center",
-    padding: 10
+    padding: 10,
   },
   emptyCardText: {
     marginTop: 10,
