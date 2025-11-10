626849576e31e20cdc66d4c471ecc29466d75a17
richmondnkrumah04 <richmondnkrumah04@gmail.com>
2025-11-10 16:02:39 +0000

    add: custom nutrition card component

--- components/NutritionCard.tsx
+++ components/NutritionCard.tsx
@@ -1,27 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { getAssetForNutrition } from '@/constants/nutritionAssets';
-import { Image } from 'expo-image';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { getAssetForNutrition } from "@/constants/nutritionAssets";
+import { Image } from "expo-image";
 
-const NutritionCard = ({quantity,label}) => {
-    const assetSource = getAssetForNutrition(label);
+const NutritionCard = ({
+  quantity,
+  label,
+}: {
+  quantity: string;
+  label: string;
+}) => {
+  const assetSource = getAssetForNutrition(label);
   return (
-    <View>
-      <Text>{label}</Text>
-      <View>
-        <Image source={assetSource} style={{width: 40, height: 40}} />
-        <Text>{quantity}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{label}</Text>
+      <View style={styles.innerContainer}>
+        <Image source={assetSource} style={{ width: 25, height: 25 }} />
+        <Text style={styles.description}>{quantity}</Text>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default NutritionCard
+export default NutritionCard;
 
 const styles = StyleSheet.create({
-    innerContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 10,
-    }
-})
+  container: {
+    padding: 5
+  },
+  innerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+    paddingTop: 5,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "500",
+    paddingLeft: 3,
+  },
+  description: {
+    alignSelf: "flex-end",
+  },
+});
